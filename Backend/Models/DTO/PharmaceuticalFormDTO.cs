47d623c5b135c4e9b2d196ddd5e394c6cc373b75
Anonimas1 <puzinas.audrius@gmail.com>
2021-04-06 17:23:34 +0300

    Add MedicamentDTO

---
+++ Backend/Models/DTO/PharmaceuticalFormDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading.Tasks;
+using Backend.Models.Database;
+using Newtonsoft.Json;
+
+namespace Backend.Models.DTO
+{
+    public class PharmaceuticalFormDTO
+    {
+        //TODO: DO I NEED ANYTHING MORE?
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        public PharmaceuticalFormDTO(PharmaceuticalForm pharmaceuticalForm)
+        {
+            Name = pharmaceuticalForm.Name;
+        }
+    }
+}
