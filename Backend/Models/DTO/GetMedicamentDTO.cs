67f0044cda7b7ff9c82f93326ec974faae4bce68
Anonimas1 <puzinas.audrius@gmail.com>
2021-04-06 18:02:33 +0300

    Add GetMedicamentDTO

---
+++ Backend/Models/DTO/GetMedicamentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models.Database;
+using Newtonsoft.Json;
+
+namespace Backend.Models.DTO
+{
+    public class GetMedicamentDTO
+    {
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
+
+        [JsonProperty("data")]
+        public MedicamentDTO Data { get; set; }
+
+        public GetMedicamentDTO(Medicament medicament)
+        {
+            Meta = new Meta();
+            Data = new MedicamentDTO(medicament);
+        }
+    }
+}
