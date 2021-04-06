8af7f61dbc5d122ce2ded1251560e3535e6e1c8c
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-06 20:17:08 +0300

    Create RequestMiddleware.cs

---
+++ Backend/Middleware/RequestMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Middleware
+{
+    public class RequestMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RequestMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+        }
+    }
+}
