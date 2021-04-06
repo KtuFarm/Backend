7d3c16f8e10d20f442669076bcd005c091e5c8a7
Anonimas1 <puzinas.audrius@gmail.com>
2021-04-06 18:03:33 +0300

    Add GET(id) for Medicament

---
+++ Backend/Controllers/MedicamentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models;
+using Backend.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class MedicamentsController : ApiControllerBase
+    {
+        public MedicamentsController(ApiContext context) : base(context)
+        {
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetMedicamentDTO>> GetMedicament(int id)
+        {
+            if (!IsValidApiRequest()) return InvalidHeaders();
+
+            var medicament = await Context.Medicaments
+                .Include(m => m.PharmaceuticalForm)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            return Ok(new GetMedicamentDTO(medicament));
+        }
+    }
+}
