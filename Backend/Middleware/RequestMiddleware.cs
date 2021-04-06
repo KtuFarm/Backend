5c857f29208c585a769e54b512519c9360c0c8c5
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-06 21:19:25 +0300

    Add header handling logic to request middleware

--- Backend/Middleware/RequestMiddleware.cs
+++ Backend/Middleware/RequestMiddleware.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Backend.Middleware
 {
     public class RequestMiddleware
     {
+        private const string ApiHeader = "X-Api-Request";
         private readonly RequestDelegate _next;
         public RequestMiddleware(RequestDelegate next)
         {
@@ -12,7 +17,25 @@ namespace Backend.Middleware
         }
         public async Task Invoke(HttpContext httpContext)
         {
+            httpContext.Request.Headers.TryGetValue(ApiHeader, out var headers);
+            if (!IsHeaderLegit(headers) || headers != "true")
+            {
+                var error = new ErrorDTO
+                {
+                    Type = 400,
+                    Title = "Invalid Headers!",
+                    Details = null
+                };
+                httpContext.Response.StatusCode = 400;
+                string jsonString = JsonConvert.SerializeObject(error, Formatting.Indented);
+                await httpContext.Response.WriteAsync(jsonString);
+                return;
+            }
             await _next(httpContext);
+        }
+        private static bool IsHeaderLegit(StringValues headers)
+        {
+            return !(headers.Count != 1 || string.IsNullOrWhiteSpace(headers.FirstOrDefault()));
         }
     }
 }
