ae734ea3e0af16afe9a6ad709ff5f76923f0b0c6
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-07 15:47:12 +0300

    Add user and worker state models

---
+++ Backend/Models/Database/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Database
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        public int Uid { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Surname { get; set; }
+
+        [Required]
+        public DateTime RegistrationDate { get; set; }
+
+        [Required]
+        public WorkerState State { get; set; }
+
+        [Required]
+        public DateTime DismissalDate { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Position { get; set; }
+    }
+}
---
+++ Backend/Models/Database/WorkerState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models.Database
+{
+    public enum WorkerStateId
+    {
+        Working,
+        OnVacation,
+        Fired
+    }
+
+    public class WorkerState
+    {
+        [Key]
+        [Required]
+        public WorkerStateId Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<User> Workers { get; set; }
+    }
+}
