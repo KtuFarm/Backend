68aa770c64e4a3cb54649eafe3ae47b6b931b74d
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-07 23:09:56 +0300

    Add User seeder

---
+++ Backend/Models/Seed/UserSeed.cs
@@ -0,0 +1,29 @@
+﻿using Backend.Models.Database;
+using System;
+using System.Linq;
+
+namespace Backend.Models.Seed
+{
+    public class UserSeed
+    {
+        public static void EnsureCreated(ApiContext context)
+        {
+            var user = context.Users.FirstOrDefault(p => p.Id == 1);
+            if (user != null) return;
+
+            context.Users.AddRange(
+                new User
+                {
+                    Id = 1,
+                    Name = "Karolis",
+                    Surname = "Balciunas",
+                    RegistrationDate = new DateTime(2020, 12, 21),
+                    WorkerStateId = WorkerStateId.Working,
+                    DismissalDate = null,
+                    Position = "Jr. Pharmacist",
+                    PharmacyId = 1
+                }
+                );
+        }
+    }
+}
