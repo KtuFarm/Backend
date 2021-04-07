ab8544073a220179be36ffa19d175185faad76c5
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-07 16:02:25 +0300

    Create WorkerStateConfiguration.cs

---
+++ Backend/Models/Configurations/WorkerStateConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Linq;
+using Backend.Models.Database;
+
+namespace Backend.Models.Configurations
+{
+    public class WorkerStateConfiguration : IEntityTypeConfiguration<WorkerState>
+    {
+        public void Configure(EntityTypeBuilder<WorkerState> builder)
+        {
+            builder.Property(ws => ws.Id).HasConversion<int>();
+            builder.HasData(
+                Enum.GetValues(typeof(WorkerStateId))
+                    .Cast<WorkerStateId>()
+                    .Select(ws => new WorkerState
+                    {
+                        Id = ws,
+                        Name = ws.ToString()
+                    })
+            );
+
+            builder
+                .HasMany(ws => ws.Workers)
+                .WithOne(u => u.State);
+        }
+    }
+}
