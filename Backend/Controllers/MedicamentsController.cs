ce5302f76f312e38442a0085e70ae02221c6c4f3
Audrius Puzinas <puzinas.audrius@gmail.com>
2021-04-07 19:44:05 +0300

    Fix exception messages

--- Backend/Controllers/MedicamentsController.cs
+++ Backend/Controllers/MedicamentsController.cs
@@ -32,6 +32,7 @@ namespace Backend.Controllers
         [HttpGet]
         public async Task<ActionResult<GetMedicamentsDTO>> GetMedicaments()
         {
+
             var medicaments = await Context.Medicaments
                 .Include(m => m.PharmaceuticalForm)
                 .Select(m => new MedicamentDTO(m))
@@ -60,13 +61,13 @@ namespace Backend.Controllers
             if (string.IsNullOrEmpty(dto.Name)) throw new ArgumentException("Name is empty!");
             if (string.IsNullOrEmpty(dto.ActiveSubstance)) throw new ArgumentException("Active substance is empty!");
             if (string.IsNullOrEmpty(dto.BarCode)) throw new ArgumentException("Bar code is empty!");
-            if (!dto.IsPrescriptionRequired.HasValue) throw new ArgumentException("Is prescription required is empty!");
-            if (!dto.IsReimbursed.HasValue) throw new ArgumentException("Is prescription required is empty!");
+            if (!dto.IsPrescriptionRequired.HasValue) throw new ArgumentException("IsPrescriptionRequired is empty!");
+            if (!dto.IsReimbursed.HasValue) throw new ArgumentException("IsReimbursed is empty!");
             if (string.IsNullOrEmpty(dto.Country)) throw new ArgumentException("Country is empty!");
-            if (dto.BasePrice == null) throw new ArgumentException("Base price is empty!");
+            if (dto.BasePrice == null) throw new ArgumentException("BasePrice is empty!");
             if (dto.Surcharge == null) throw new ArgumentException("Surcharge is empty!");
-            if (!dto.IsSellable.HasValue) throw new ArgumentException("Is sellable is empty!");
-            if (dto.ReimbursePercentage == null) throw new ArgumentException("Reimburse percentage is empty!");
+            if (!dto.IsSellable.HasValue) throw new ArgumentException("IsSellable is empty!");
+            if (dto.ReimbursePercentage == null) throw new ArgumentException("ReimbursePercentage is empty!");
         }
     }
 }
