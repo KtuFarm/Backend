dddc8deae90363e16135a4a3f405ec32d103059b
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-08 18:17:13 +0300

    Create CreateUserDTO.cs

---
+++ Backend/Models/DTO/CreateUserDTO.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Backend.Models.DTO
+{
+    public class CreateUserDTO
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("surname")]
+        public string Surname { get; set; }
+
+        [JsonProperty("registrationDate")]
+        public DateTime RegistrationDate { get; set; }
+
+        [JsonProperty("position")]
+        public string Position { get; set; }
+
+        [JsonProperty("employeeState")]
+        public int? EmployeeState { get; set; }
+    }
+}
