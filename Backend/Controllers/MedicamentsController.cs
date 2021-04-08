5708b7a1cb6d3c57b5abffc572307f73841d7bfb
Audrius Puzinas <puzinas.audrius@gmail.com>
2021-04-08 17:18:44 +0300

    Add Edit method and validation

--- Backend/Controllers/MedicamentsController.cs
+++ Backend/Controllers/MedicamentsController.cs
@@ -55,6 +55,26 @@ namespace Backend.Controllers
             return Created();
         }
 
+        [HttpPut("{id}")]
+        public async Task<ActionResult> EditMedicament(int id, [FromBody] EditMedicamentDTO dto)
+        {
+            ValidateEditMedicamentDTO(dto);
+            var medicament = await Context.Medicaments.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (medicament == null) return ApiNotFound("Medicament not found!");
+
+            medicament.IsPrescriptionRequired = (bool) dto.IsPrescriptionRequired;
+            medicament.IsReimbursed = (bool) dto.IsReimbursed;
+            medicament.BasePrice = (decimal) dto.BasePrice;
+            medicament.Surcharge = (double) dto.Surcharge;
+            medicament.IsSellable = (bool) dto.IsSellable;
+            medicament.ReimbursePercentage = (int) dto.ReimbursePercentage;
+
+            await Context.SaveChangesAsync();
+
+            return Ok();
+        }
+
         [AssertionMethod]
         private static void ValidateCreateMedicamentDTO(CreateMedicamentDTO dto)
         {
@@ -64,6 +84,17 @@ namespace Backend.Controllers
             if (!dto.IsPrescriptionRequired.HasValue) throw new ArgumentException("IsPrescriptionRequired is empty!");
             if (!dto.IsReimbursed.HasValue) throw new ArgumentException("IsReimbursed is empty!");
             if (string.IsNullOrEmpty(dto.Country)) throw new ArgumentException("Country is empty!");
+            if (dto.BasePrice == null) throw new ArgumentException("BasePrice is empty!");
+            if (dto.Surcharge == null) throw new ArgumentException("Surcharge is empty!");
+            if (!dto.IsSellable.HasValue) throw new ArgumentException("IsSellable is empty!");
+            if (dto.ReimbursePercentage == null) throw new ArgumentException("ReimbursePercentage is empty!");
+        }
+
+        [AssertionMethod]
+        private static void ValidateEditMedicamentDTO(EditMedicamentDTO dto)
+        {
+            if (!dto.IsPrescriptionRequired.HasValue) throw new ArgumentException("IsPrescriptionRequired is empty!");
+            if (!dto.IsReimbursed.HasValue) throw new ArgumentException("IsReimbursed is empty!");
             if (dto.BasePrice == null) throw new ArgumentException("BasePrice is empty!");
             if (dto.Surcharge == null) throw new ArgumentException("Surcharge is empty!");
             if (!dto.IsSellable.HasValue) throw new ArgumentException("IsSellable is empty!");
