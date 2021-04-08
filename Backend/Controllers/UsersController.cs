c7f8da1a4b3bb2262482134b227f308a8b4b899a
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-04-08 17:21:12 +0300

    Create UsersController.cs

---
+++ Backend/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UsersController : ApiControllerBase
+    {
+        public UsersController(ApiContext context) : base(context) {}
+
+    }
+}
