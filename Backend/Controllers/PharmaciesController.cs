ab44ef1eca2498da14d048a43d570f9a933ecbd6
wizacass <visak.pet0@gmail.com>
2021-04-25 20:16:40 +0300

    Implement generic List DTO in Pharmacies Controller

--- Backend/Controllers/PharmaciesController.cs
+++ Backend/Controllers/PharmaciesController.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Backend.Exceptions;
+using Backend.Models.Common.DTO;
 
 namespace Backend.Controllers
 {
@@ -31,24 +32,24 @@ namespace Backend.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<GetPharmaciesDTO>> GetPharmacies()
+        public async Task<ActionResult<GetListDTO<PharmacyDTO>>> GetPharmacies()
         {
             var pharmacies = await Context.Pharmacies
                 .Select(p => new PharmacyDTO(p))
                 .ToListAsync();
 
-            return Ok(new GetPharmaciesDTO(pharmacies));
+            return Ok(new GetListDTO<PharmacyDTO>(pharmacies));
         }
 
         [HttpGet("all")]
-        public async Task<ActionResult<GetPharmaciesDTO>> GetAllPharmacies()
+        public async Task<ActionResult<GetListDTO<PharmacyDTO>>> GetAllPharmacies()
         {
             var pharmacies = await Context.Pharmacies
                 .IgnoreQueryFilters()
                 .Select(p => new PharmacyDTO(p))
                 .ToListAsync();
 
-            return Ok(new GetPharmaciesDTO(pharmacies));
+            return Ok(new GetListDTO<PharmacyDTO>(pharmacies));
         }
 
         [HttpGet("{id}")]
@@ -128,24 +129,26 @@ namespace Backend.Controllers
         }
 
         [HttpGet("{id}/products")]
-        public async Task<ActionResult<GetProductBalancesDTO>> GetProductBalances(int id)
+        public async Task<ActionResult<GetListDTO<ProductBalanceDTO>>> GetProductBalances(int id)
         {
             var products = await Context.ProductBalances
                 .Include(pb => pb.Medicament)
                 .Where(pb => pb.PharmacyId == id)
+                .Select(p => new ProductBalanceDTO(p))
                 .ToListAsync();
 
-            return Ok(new GetProductBalancesDTO(products));
+            return Ok(new GetListDTO<ProductBalanceDTO>(products));
         }
 
         [HttpGet("{id}/transactions")]
-        public async Task<ActionResult<GetTransactionsDTO>> GetTransactions(int id)
+        public async Task<ActionResult<GetListDTO<TransactionDTO>>> GetTransactions(int id)
         {
             var transactions = await Context.Transactions
                 .Where(t => t.PharmacyId == id)
+                .Select(t => new TransactionDTO(t))
                 .ToListAsync();
 
-            return Ok(new GetTransactionsDTO(transactions));
+            return Ok(new GetListDTO<TransactionDTO>(transactions));
         }
     }
 }
---
+++ Backend/Models/DTO/Common/GetListDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Backend.Models.Common.DTO
+{
+    public class GetListDTO<T>
+    {
+        [JsonProperty("meta")]
+        public Meta Meta { get; set; }
+
+        [JsonProperty("data")]
+        public IEnumerable<T> Data { get; set; }
+
+        public GetListDTO(IEnumerable<T> data)
+        {
+            Meta = new Meta();
+            Data = data;
+        }
+    }
+}
