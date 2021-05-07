12b248e5247809bea24f3e9e082fa9fe4470c908
Audrius Puzinas <puzinas.audrius@gmail.com>
2021-05-07 20:32:01 +0300

    Add required models

---
+++ Backend/Models/Database/OrderState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models.OrderEntity;
+
+namespace Backend.Models.Database
+{
+    public enum OrderStateId
+    {
+        Created = 1,
+        Approved,
+
+        Ready,
+        InTransit,
+        Delivered,
+
+        Returned
+    }
+
+    public class OrderState
+    { 
+        [Key]
+        [Required]
+        public OrderStateId Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
---
+++ Backend/Models/OrderEntity/Order.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Models.Common;
+using Backend.Models.Database;
+using Backend.Models.ProductBalanceEntity;
+using Backend.Models.UserEntity;
+using Backend.Models.WarehouseEntity;
+
+namespace Backend.Models.OrderEntity
+{
+    public class Order : ISoftDeletable
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string AddressFrom { get; set; }
+
+        [StringLength(255)]
+        [Required]
+        public string AddressTo { get; set; }
+
+        [Required]
+        public DateTime DeliveryDate { get; set; }
+
+        [Required]
+        public OrderStateId OrderStateId { get; set; }
+
+        [Required]
+        public OrderState OrderState { get; set; }
+
+        [Required]
+        public double Total { get; set; }
+
+        public int? WarehouseId { get; set; }
+
+        [Required]
+        public Warehouse Warehouse { get; set; }
+
+        public int? PharmacistId { get; set; }
+
+        [Required]
+        public User Pharmacist { get; set; }
+
+        public int? CourierId { get; set; }
+
+        [Required]
+        public User Courier { get; set; }
+
+        [Required]
+        [DefaultValue(false)]
+        public bool IsSoftDeleted { get; set; } = false;
+    }
+}
