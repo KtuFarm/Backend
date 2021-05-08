56a74dea51981919676655bc62362185945d8b3c
wizacass <visak.pet0@gmail.com>
2021-05-08 23:22:02 +0300

    Implement logic to see Warehouse proucts balances

--- Backend/Controllers/WarehousesController.cs
+++ Backend/Controllers/WarehousesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Models.ProductBalanceEntity.DTO;
 
 namespace Backend.Controllers
 {
@@ -38,6 +39,18 @@ namespace Backend.Controllers
             if (warehouse == null) return ApiNotFound(ApiErrorSlug.ResourceNotFound, ModelName);
 
             return Ok(new GetObjectDTO<WarehouseDTO>(warehouse));
+        }
+
+        [HttpGet("{id}/products")]
+        public async Task<ActionResult<GetListDTO<ProductBalanceDTO>>> GetProductBalances(int id)
+        {
+            var products = await Context.ProductBalances
+                .Include(pb => pb.Medicament)
+                .Where(pb => pb.WarehouseId == id)
+                .Select(p => new ProductBalanceDTO(p))
+                .ToListAsync();
+
+            return Ok(new GetListDTO<ProductBalanceDTO>(products));
         }
     }
 }
