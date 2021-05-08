0e3589ba42687fe218c8e6bfe3783d0c70644576
wizacass <visak.pet0@gmail.com>
2021-05-08 23:06:31 +0300

    Create new order DTO

---
+++ Backend/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Backend.Models;
+using Backend.Models.DTO;
+using Backend.Models.OrderEntity.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrdersController : ApiControllerBase
+    {
+        public OrdersController(ApiContext context) : base(context) { }
+
+        [HttpPost]
+        public ActionResult<GetObjectDTO<CreateOrderDTO>> CreateOrder([FromBody] CreateOrderDTO dto)
+        {
+            throw new NotImplementedException();
+            
+
+            return Ok(new GetObjectDTO<CreateOrderDTO>(dto));
+        }
+    }
+}
---
+++ Backend/Models/OrderEntity/DTO/CreateOrderDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Backend.Models.DTO;
+using Newtonsoft.Json;
+
+namespace Backend.Models.OrderEntity.DTO
+{
+    public class CreateOrderDTO
+    {
+        [JsonProperty("warehouseId")]
+        public int WarehouseId { get; set; }
+
+
+        [JsonProperty("products")]
+        public List<TransactionProductDTO> Products { get; set; }
+    }
+}
