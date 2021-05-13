8b06977e8603ee3592f3ac2e03d1a099f5e7c429
wizacass <visak.pet0@gmail.com>
2021-05-13 20:48:04 +0300

    Enrich user dto to include workplace id

--- Backend/Models/UserEntity/DTO/UserFullDTO.cs
+++ Backend/Models/UserEntity/DTO/UserFullDTO.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace Backend.Models.UserEntity.DTO
 {
-    public class UserFullDTO: UserDTO
+    public class UserFullDTO : UserDTO
     {
         [JsonProperty("registrationDate")]
         public DateTime RegistrationDate { get; set; }
@@ -14,11 +14,19 @@ namespace Backend.Models.UserEntity.DTO
         [JsonProperty("employeeState")]
         public string EmployeeState { get; set; }
 
+        [JsonProperty("pharmacyId")]
+        public int? PharmacyId { get; set; }
+
+        [JsonProperty("warehouseId")]
+        public int? WarehouseId { get; set; }
+
         public UserFullDTO(User user) : base(user)
         {
             RegistrationDate = user.RegistrationDate;
             DismissalDate = user.DismissalDate;
             EmployeeState = user.EmployeeStateId.ToString();
+            PharmacyId = user.PharmacyId;
+            WarehouseId = user.WarehouseId;
         }
     }
 }
