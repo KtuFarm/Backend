ab61d876326f5f602a8963f3987633ec25fd6f5c
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-15 22:15:04 +0300

    Add migrations

---
+++ Backend/Migrations/20210515191442_AddReports.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace Backend.Migrations
+{
+    public partial class AddReports : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ReportTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Code = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false),
+                    DateFrom = table.Column<DateTime>(type: "datetime", nullable: false),
+                    DateTo = table.Column<DateTime>(type: "datetime", nullable: false),
+                    OrderCount = table.Column<int>(type: "int", nullable: false),
+                    TransactionCount = table.Column<int>(type: "int", nullable: false),
+                    TotalRevenue = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    RevenueInCash = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    GenerationDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    TotalOrderAmount = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    Profit = table.Column<decimal>(type: "decimal(18, 2)", nullable: false),
+                    ReportTypeId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    PharmacyId = table.Column<int>(type: "int", nullable: false),
+                    IsSoftDeleted = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reports_Pharmacies_PharmacyId",
+                        column: x => x.PharmacyId,
+                        principalTable: "Pharmacies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reports_ReportTypes_ReportTypeId",
+                        column: x => x.ReportTypeId,
+                        principalTable: "ReportTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reports_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ReportTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Daily" });
+
+            migrationBuilder.InsertData(
+                table: "ReportTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Weekly" });
+
+            migrationBuilder.InsertData(
+                table: "ReportTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Custom" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_PharmacyId",
+                table: "Reports",
+                column: "PharmacyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_ReportTypeId",
+                table: "Reports",
+                column: "ReportTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reports_UserId",
+                table: "Reports",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reports");
+
+            migrationBuilder.DropTable(
+                name: "ReportTypes");
+        }
+    }
+}
