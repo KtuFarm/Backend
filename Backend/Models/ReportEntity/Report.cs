aa91f0e0464f655021df51aecf2c6139d2a76799
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-15 20:47:11 +0300

    Add report model

---
+++ Backend/Models/ReportEntity/Report.cs
@@ -0,0 +1,69 @@
+﻿using Backend.Models.Common;
+using Backend.Models.Database;
+using Backend.Models.PharmacyEntity;
+using Backend.Models.UserEntity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models.ReportEntity
+{
+    public class Report : ISoftDeletable
+    {
+        [Key]
+        [Required]
+        public int Id { get; init; }
+
+        [Required]
+        [StringLength(255)]
+        public string Code { get; set; }
+
+        [Required]
+        public DateTime DateFrom { get; set; }
+
+        [Required]
+        public DateTime DateTo { get; set; }
+
+        [Required]
+        public int OrderCount { get; set; }
+
+        [Required]
+        public int TransactionCount { get; set; }
+
+        [Required]
+        public decimal TotalRevenue { get; set; } = 0.00M;
+
+        [Required]
+        public decimal RevenueInCash { get; set; } = 0.00M;
+
+        [Required]
+        public DateTime GenerationDate { get; set; }
+
+        [Required]
+        public decimal TotalOrderAmount { get; set; } = 0.00M;
+
+        [Required]
+        public decimal Profit { get; set; }
+
+        [Required]
+        public ReportTypeId ReportTypeId { get; set; }
+
+        [Required]
+        public ReportType ReportType { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+
+        public int PharmacyId { get; set; }
+
+        public Pharmacy Pharmacy { get; set; }
+
+        [Required]
+        [DefaultValue(false)]
+        public bool IsSoftDeleted { get; set; } = false;
+    }
+}
