ca5eb5bb95e1d646d53e4baff8c68b9aedf754ad
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-16 23:26:28 +0300

    Fix missing parameters in DTOs

--- Backend/Models/ReportEntity/DTO/ReportDTO.cs
+++ Backend/Models/ReportEntity/DTO/ReportDTO.cs
@@ -1,24 +1,27 @@
 ﻿using Newtonsoft.Json;
-using System;
 
 namespace Backend.Models.ReportEntity.DTO
 {
     public class ReportDTO
     {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
         [JsonProperty("code")]
         public string Code { get; set; }
 
         [JsonProperty("dateFrom")]
-        public DateTime DateFrom { get; set; }
+        public string DateFrom { get; set; }
 
         [JsonProperty("dateTo")]
-        public DateTime DateTo { get; set; }
+        public string DateTo { get; set; }
 
         public ReportDTO(Report report)
         {
+            Id = report.Id;
             Code = report.Code;
-            DateFrom = report.DateFrom;
-            DateTo = report.DateTo;
+            DateFrom = report.DateFrom.ToString("O");
+            DateTo = report.DateTo.ToString("O");
         }
     }
 }
