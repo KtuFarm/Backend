c43c837632f349ca428e64f0221a0fafbafca4e4
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-16 18:59:37 +0300

    Add create DTO validation tests

---
+++ BackendTests/ReportValidatorTests.cs
@@ -0,0 +1,47 @@
+﻿using Backend.Exceptions;
+using Backend.Models.ReportEntity.DTO;
+using Backend.Services.Validators.ReportDTOValidator;
+using NUnit.Framework;
+using System;
+using static NUnit.Framework.Assert;
+
+namespace BackendTests
+{
+    class ReportValidatorTests
+    {
+        private IReportDTOValidator _validator;
+
+        private static CreateReportDTO ValidCreateDto =>
+            new()
+            {
+                DateFrom = new DateTime(2021, 5, 15),
+                DateTo = new DateTime(2021, 5, 20),
+                ReportTypeId = 1
+            };
+
+        [SetUp]
+        public void Setup()
+        {
+            _validator = new ReportDTOValidator();
+        }
+
+        [Test]
+        public void TestValidDto()
+        {
+            var dto = ValidCreateDto;
+
+            _validator.ValidateCreateReportDTO(dto);
+
+            Pass();
+        }
+
+        [Test]
+        public void TestInvalidReportTypeId()
+        {
+            var dto = ValidCreateDto;
+            dto.ReportTypeId = -1;
+
+            Throws<DtoValidationException>(() => _validator.ValidateCreateReportDTO(dto));
+        }
+    }
+}
