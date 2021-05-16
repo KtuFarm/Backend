d8e15c2531b293f64ba08a31729a406fd4d66295
Karolis Balčiūnas <ka.balciunas@ktu.edu>
2021-05-16 21:07:08 +0300

    Add new migrations

---
+++ Backend/Migrations/20210516180323_UpdateReportFieldNaming.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Backend.Migrations
+{
+    public partial class UpdateReportFieldNaming : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalOrderAmount",
+                table: "Reports");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalOrderSum",
+                table: "Reports",
+                type: "decimal(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalOrderSum",
+                table: "Reports");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalOrderAmount",
+                table: "Reports",
+                type: "decimal(18, 2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
