588ebc4ca0f170be494a1cf9aaee6fb06772dbab
wizacass <visak.pet0@gmail.com>
2021-10-28 21:14:00 +0300

    Create parametrized test

--- BackendTests/PriceCalculationTests.cs
+++ BackendTests/PriceCalculationTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Backend.Models.MedicamentEntity;
 using NUnit.Framework;
 
@@ -5,41 +6,33 @@ namespace BackendTests
 {
     public class PriceCalculationTests
     {
-        [Test]
-        public void TestBasePriceWithSurcharge()
+        private static IEnumerable<TestCaseData> PriceTestData()
         {
-            var m = new Medicament
+            yield return new TestCaseData(new Medicament
             {
                 BasePrice = 20M,
                 Surcharge = 10
-            };
+            }, 22.00M);
 
-            Assert.AreEqual(m.CalculatePriceReimbursed(), 22);
-        }
-
-        [Test]
-        public void TestBasePriceWithReimburse()
-        {
-            var m = new Medicament
+            yield return new TestCaseData(new Medicament
             {
                 BasePrice = 20M,
                 ReimbursePercentage = 10
-            };
+            }, 18.00M);
 
-            Assert.AreEqual(m.CalculatePriceReimbursed(), 18);
-        }
-
-        [Test]
-        public void TestPriceWithSurchargeAndReimburse()
-        {
-            var m = new Medicament
+            yield return new TestCaseData(new Medicament
             {
                 BasePrice = 20M,
                 Surcharge = 50,
                 ReimbursePercentage = 10
-            };
+            }, 27.00M);
+        }
 
-            Assert.AreEqual(m.CalculatePriceReimbursed(), 27);
+
+        [Test, TestCaseSource(nameof(PriceTestData))]
+        public void TestPriceCalculation(Medicament m, decimal expectedPrice)
+        {
+            Assert.AreEqual(expectedPrice, m.CalculatePriceReimbursed());
         }
     }
 }
